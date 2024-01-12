936c6d89b85aa102fcde84ee6dfdac2f17c792ef
leonel <leonelyanza2@gmail.com>
2024-01-12 13:20:22 -0500

    Corrección ejercicio #00

---
+++ 00_Leo.py
@@ -0,0 +1,23 @@
+# https://www.python.org/
+# Este es un comentario de una linea
+'''este es un comentario 
+de multiples
+lineas'''
+
+my_variable = 'my variable'
+
+#tipos de datos en python 
+
+#entero
+int = 5
+#flotante
+float = 5.12
+#boleano (false, true)
+bool = True
+#cadena de texto
+str = 'Leonel'
+# lista
+list = [1,2,3,4]
+# imprime mensaje en pantalla
+
+print('¡Hola, python¡')
